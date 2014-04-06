045d9dc359b3d4613da5f030ec9286693910474e
Aanjaneya Mishra <tomtum2004-linux@yahoo.com>
2014-04-06 23:53:57 +0100

    changed octopus user to o1 which is boa default.

--- cookbooks/barracuda/recipes/default.rb
+++ cookbooks/barracuda/recipes/default.rb
@@ -9,8 +9,8 @@ execute "/tmp/BOA.sh" do
   creates "/usr/local/bin/boa"
 end
 
-execute "Run the BOA Installer c1" do
-  command "boa in-stable local openamsri@gmail.com c1 mini"
+execute "Run the BOA Installer o1" do
+  command "boa in-stable local openamsri@gmail.com mini"
 end
 
 # execute "Run the BOA Installer o2" do
@@ -23,45 +23,45 @@ end
 
 #(1..3).each do |boa_user|
 
-user "c1" do
+user "o1" do
 supports :manage_home => true
-home "/data/disk/c1"
+home "/data/disk/o1"
 shell "/bin/bash"
 end
 
-directory "/data/disk/c1/.ssh" do
-owner "c1"
+directory "/data/disk/o1/.ssh" do
+owner "o1"
 group "users"
 mode 00700
 recursive true
 end
 
 execute "Add ssh key to user" do
-command "ssh-keygen -b 4096 -t rsa -N \"\" -f /data/disk/c1/.ssh/id_rsa"
-creates "/data/disk/c1/.ssh/id_rsa"
+command "ssh-keygen -b 4096 -t rsa -N \"\" -f /data/disk/o1/.ssh/id_rsa"
+creates "/data/disk/o1/.ssh/id_rsa"
 end
 
-file "/data/disk/c1/.ssh/id_rsa" do
-owner "c1"
+file "/data/disk/o1/.ssh/id_rsa" do
+owner "o1"
 group "users"
 mode 00600
 end
 
-file "/data/disk/c1/.ssh/id_rsa.pub" do
-owner "c1"
+file "/data/disk/o1/.ssh/id_rsa.pub" do
+owner "o1"
 group "users"
 mode 00600
 end
 
 # Only necessary as long as there is a but
-#remote_file "/tmp/fix-remote-import-hostmaster-c1.patch" do
-#source "https://raw.github.com/lsolesen/boa-vagrant/master/patches/fix-remote-import-hostmaster-c1.patch"
+#remote_file "/tmp/fix-remote-import-hostmaster-o1.patch" do
+#source "https://raw.github.com/lsolesen/boa-vagrant/master/patches/fix-remote-import-hostmaster-o1.patch"
 #mode 00755
 #end
 
 #execute "Apply Remote Import hostmaster patch" do
-#cwd "/data/disk/c1/.drush/provision/remote_import"
-#command "patch -p1 < /tmp/fix-remote-import-hostmaster-c1.patch"
+#cwd "/data/disk/o1/.drush/provision/remote_import"
+#command "patch -p1 < /tmp/fix-remote-import-hostmaster-o1.patch"
 #end
 
 # end
